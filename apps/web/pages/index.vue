1624aef7af664ffe53a8e97ece5ba6f96325d1af
Christopher Reeve <work@creeve.me>
2023-12-07 08:46:47 +0700

    feat: add error

--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -5,24 +5,20 @@ const { getItems } = useDirectusItems();
 const {
     data: blogList,
     pending: loading,
-    execute,
-} = await useAsyncData(
-    "bloglist",
-    () =>
-        getItems<Blog>({
-            collection: "blog",
-        }),
-    {
-        immediate: true,
-    }
+    error,
+} = await useAsyncData("bloglist", () =>
+    getItems<Blog>({
+        collection: "blog",
+    })
 );
 </script>
 
 <template>
     <div>
         <div>
-            <p v-if="loading">Loading...</p>
-            <pre v-else>{{ blogList }}</pre>
+            {{ error }}
         </div>
+        <p v-if="loading">Loading...</p>
+        <pre v-else>{{ blogList }}</pre>
     </div>
 </template>
